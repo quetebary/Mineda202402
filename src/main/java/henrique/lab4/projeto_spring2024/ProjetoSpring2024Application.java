adb5c7d57eda873dcf36f2a9e56c672a6f5cd137
Henrique de Castro Silva <fatec.henrique@gmail.com>
2024-09-18 20:53:42 -0300

    teste1213

---
+++ src/main/java/henrique/lab4/projeto_spring2024/ProjetoSpring2024Application.java
@@ -0,0 +1,13 @@
+package henrique.lab4.projeto_spring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/henrique/lab4/projeto_spring2024/ProjetoSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package henrique.lab4.projeto_spring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
